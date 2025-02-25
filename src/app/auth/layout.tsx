045dd11d5b6b7838da78e9070b50e422f6da5f25
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-25 12:04:11 +0700

    fix: auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from "react";
-
-export const AuthLayout = ({ children }: { children: ReactNode }) => {
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <main className="flex justify-center w-full max-w-7xl mx-auto py-24">
       {children}
     </main>
   );
-};
-
-export default AuthLayout;
+}
